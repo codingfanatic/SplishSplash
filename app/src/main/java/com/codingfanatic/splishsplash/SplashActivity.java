ae7ea14c881ecb9a3560fedab0f5210d843842e0
codingfanatic <lclarkerichard@yahoo.com>
2020-07-26 20:29:13 -0400

    Finalized Splash Screen Adding files from my finished Splash Screen sample

---
+++ app/src/main/java/com/codingfanatic/splishsplash/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.codingfanatic.splishsplash;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
