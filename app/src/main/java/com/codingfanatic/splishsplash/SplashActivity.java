6fa78fc8c95451dd8796397ee8db870cb1d564bf
codingfanatic <lclarkerichard@yahoo.com>
2020-08-09 21:55:47 -0400

    Added a Splash Screen to the app I created a Splash Screen using an image from JoJo's. I resized it to 250x250, created a drawable resource, and updated the Splash Activity accordingly.

--- app/src/main/java/com/codingfanatic/splishsplash/SplashActivity.java
+++ app/src/main/java/com/codingfanatic/splishsplash/SplashActivity.java
@@ -10,7 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
 
@@ -22,6 +22,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
-        }, 5000);
+        }, 3000);
     }
 }
